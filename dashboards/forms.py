928b7a6ebf5337a5df88f3fa07c24d435ec518c4
MosesMawoneni <98585395+MosesMawoneni@users.noreply.github.com>
2023-10-29 16:10:52 +0200

    dashboards user management

--- dashboards/forms.py
+++ dashboards/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 from blogs.models import Blog, Category
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class CategoryForm(forms.ModelForm):
@@ -20,4 +22,36 @@ class BlogPostForm(forms.ModelForm):
             "blog_body",
             "status",
             "is_featured",
+        )
+
+
+class AddUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "groups",
+            "user_permissions",
+        )
+
+
+class EditUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "groups",
+            "user_permissions",
         )

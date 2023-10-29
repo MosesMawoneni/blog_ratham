ad96b335b5581f5b745b33532f500012ff466664
MosesMawoneni <98585395+MosesMawoneni@users.noreply.github.com>
2023-10-29 21:18:07 +0200

    added permissions

--- blogs/views.py
+++ blogs/views.py
@@ -8,7 +8,7 @@ def posts_by_category(request, category_id):
     posts = Blog.objects.filter(status="Published", category=category_id)
     category = get_object_or_404(Category, id=category_id)
     context = {"posts": posts, "category": category}
-    return render(request, "posts_by_category.html", context)
+    return render(request, "dashboard/posts_by_category.html", context)
 
 
 def blogs(request, slug):
